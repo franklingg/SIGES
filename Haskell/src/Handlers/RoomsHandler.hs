baa9a65cb166d20475cb7c331af6eb00e2accff2
sheilapaiva <sheila.paiva@ccc.ufcg.edu.br>
2021-04-20 22:28:07 -0300

    Adiciona a documentacao do arquivo RoomsHandler.hs

--- Haskell/src/Handlers/RoomsHandler.hs
+++ Haskell/src/Handlers/RoomsHandler.hs
@@ -1,14 +1,19 @@
 module Handlers.RoomsHandler where
 
+-- importa a entidade Manager
 import Manager
+-- importa a entidade DataHandler
 import Handlers.DataHandler
+-- importa a entidade List
 import Data.List
 
+-- Room e uma instancia da classe de tipo Show
 instance Show Room where
     show(Room codeRoom _ _ capRoom localRoom catRoom) = "Dados da sala:                   \n\
                                                         \Código: "++ codeRoom ++         "\n\
                                                         \Capacidade: "++ show capRoom
 
+-- Reservation e uma instancia da classe de tipo Show
 instance Show Reservation where
     show(Reservation requester description startTime finishTime) = "Dados da reserva:                 \n\
                                                                    \Início: "++ show startTime ++    "\n\
@@ -16,18 +21,28 @@ instance Show Reservation where
                                                                    \Responsável: " ++ requester ++   "\n\
                                                                    \Motivo: "++ description
 
+-- Reservation e uma instancia da classe de tipo Ord
 instance Ord Reservation where
     compare res1 res2 = if startTime res1 <= startTime res2 then LT else GT
 
+{-
+   Funcao para arranjar o tempo.
+-}
 makeTime :: (Integer, Int, Int, Int, Int) -> LocalTime
 makeTime (a, m, d, h, min) = time
     where (Just clock) = makeTimeOfDayValid h min 00
           calendar = fromGregorian a m d
           time = LocalTime calendar clock
 
+{-
+   Funcao para verificar se esta livre.
+-}
 isFree :: Room -> LocalTime -> Bool
 isFree room newTime = not $ any (\reservation -> (startTime reservation <= newTime) && (finishTime reservation >= newTime)) (schedule room)
 
+{-
+   Funcao para realizar uma reserva.
+-}
 makeReservation :: String -> String -> String -> (Integer, Int, Int, Int, Int) -> (Integer, Int, Int, Int, Int) -> IO Bool
 makeReservation codeRoom userName descriptionStr startTimeTuple finishTimeTuple = do
     let startTimeReservation = makeTime startTimeTuple
@@ -45,6 +60,9 @@ makeReservation codeRoom userName descriptionStr startTimeTuple finishTimeTuple 
         else
             return False
 
+{-
+   Funcao para deletar uma reserva.
+-}
 deleteReservation :: String -> String -> (Integer, Int, Int, Int, Int) -> IO Bool
 deleteReservation codeRoom userName startTimeTuple = do
     let timeReservation = makeTime startTimeTuple
@@ -60,6 +78,9 @@ deleteReservation codeRoom userName startTimeTuple = do
 
         else return False
 
+{-
+   Funcao para editar uma reserva.
+-}
 editReservation :: String -> String -> (Integer, Int, Int, Int, Int) -> (Integer, Int, Int, Int, Int) -> (Integer, Int, Int, Int, Int) -> IO Bool
 editReservation codeRoom userName currentStartTimeTuple newStartTimeTuple newFinishTimeTuple = do
     (Just room) <- getRoom codeRoom
@@ -82,10 +103,16 @@ editReservation codeRoom userName currentStartTimeTuple newStartTimeTuple newFin
         else do
             return False
 
+{-
+   Funcao para retirar as reservas que ja passaram do tempo.
+-}
 cleanReservations :: LocalTime -> Room -> Room
 cleanReservations timeNow room = Room{code=code room, schedule=newSchedule, resources=resources room, capacity=capacity room, localization=localization room, category=category room}
     where newSchedule = filter (\reservation -> finishTime reservation > timeNow) (schedule room)
 
+{-
+   Funcao para retirar todas as reservas que ja passaram do tempo.
+-}
 cleanAllReservations :: IO Bool
 cleanAllReservations = do
     utcTimeNow <- getCurrentTime
@@ -93,6 +120,9 @@ cleanAllReservations = do
     let timeNow = utcToLocalTime timeZone utcTimeNow
     updateAllRooms (cleanReservations timeNow)        
 
+{-
+   Funcao para criar um relatorio para a sala.
+-}
 createReportForTheRoom :: (Integer, Int, Int) -> Room -> String
 createReportForTheRoom (a, m, d) room = result
     where calendar = fromGregorian a m d
@@ -100,6 +130,9 @@ createReportForTheRoom (a, m, d) room = result
           reservationsList = ("Relatório de ocupação para a sala " ++ code room ++ " no dia: " ++ show calendar ++ ":\n\n"): map show reservations
           result = intercalate "\n" reservationsList
 
+{-
+   Funcao para criar um relatorio para o dia.
+-}
 createReportForTheDay :: (Integer, Int, Int) -> IO String
 createReportForTheDay calendarTuple = do
     allRooms <- fetchRooms
@@ -108,6 +141,9 @@ createReportForTheDay calendarTuple = do
 
     return result
 
+{-
+   Funcao para procurar sala por categoria.
+-}
 searchRoomsCategory :: Maybe RoomCategory -> IO [Room]
 searchRoomsCategory catRoom = do
     allRooms <- fetchRooms
@@ -117,6 +153,9 @@ searchRoomsCategory catRoom = do
     else do
         return allRooms
 
+{-
+   Funcao para procurar sala por capacidade.
+-}
 searchRoomsCapacity :: Maybe Int -> IO [Room]
 searchRoomsCapacity capRoom = do
     allRooms <- fetchRooms
@@ -126,6 +165,9 @@ searchRoomsCapacity capRoom = do
     else do
         return allRooms
 
+{-
+   Funcao para procurar sala por tempo.
+-}
 searchRoomsTime :: Maybe (Integer, Int, Int, Int, Int) -> Maybe (Integer, Int, Int, Int, Int) -> IO [Room]
 searchRoomsTime startTimeTuple finishTimeTuple = do
     allRooms <- fetchRooms
@@ -137,14 +179,23 @@ searchRoomsTime startTimeTuple finishTimeTuple = do
     else do
         return allRooms
 
+{-
+   Funcao para verificar se o recurso e suficiente.
+-}
 resourceIsEnough :: Room -> Resource -> Bool
 resourceIsEnough room r | any (\roomResource -> (resourceKind roomResource == resourceKind r) && (resourceQuantity roomResource >= resourceQuantity r)) (resources room) = True
                         | otherwise = False
 
+{-
+   Funcao para verificar se contem recurso.
+-}
 containsResources :: [Resource] -> Room -> Bool
 containsResources [] _ = True
 containsResources rs room = all (resourceIsEnough room) rs
 
+{-
+   Funcao para pesquisar recursos das salas.
+-}
 searchRoomsResources :: Maybe [Resource] -> IO [Room]
 searchRoomsResources resourcesNeeded = do
     allRooms <- fetchRooms
@@ -154,6 +205,9 @@ searchRoomsResources resourcesNeeded = do
     else do
         return allRooms
 
+{-
+   Funcao para pesquisar salas com recursos combinados.
+-}
 searchRoomsCombined :: Maybe RoomCategory -> Maybe Int -> Maybe (Integer, Int, Int, Int, Int) -> Maybe (Integer, Int, Int, Int, Int) -> Maybe [Resource] -> IO [Room]
 searchRoomsCombined catRoom capRoom startTimeTuple finishTimeTuple resourcesNeeded = do
     filteredCategory <- searchRoomsCategory catRoom
