18cab9bf76d6a81c7760023f8202ccedb27ca46b
Franklin Regis <franklin.oliveira@ccc.ufcg.edu.br>
2021-04-25 20:58:48 -0300

    docs

--- Prolog/README.md
+++
@@ -1,5 +0,0 @@
-## SIPRO
-
-Prolog implementation of SIGES
-
-visit https://github.com/franklingg/SIGES for more details.
--- Prolog/test/testSuite.pl
+++
@@ -1,8 +0,0 @@
-:- module(testSuite,[init/0]).
-:- use_module(library(plunit)).
-
-init :- 
-    consult('test/Handlers/userHandler.plt'),
-    consult('test/Handlers/utils.plt'),
-    run_tests.
-
