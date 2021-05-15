930a298f37a1063288b9c6096f415295cb3729cc
deiltonlopes <deilton.lopes@ccc.ufcg.edu.br>
2021-05-15 20:56:11 -0300

    Minor fixes.

--- Prolog/test/testSuite.pl
+++ Prolog/test/testSuite.pl
@@ -2,8 +2,8 @@
 :- use_module(library(plunit)).
 
 init :- 
-    % consult("test/Handlers/userHandler.plt"),
-    % consult("test/Handlers/utils.plt"),
+    consult("test/Handlers/userHandler.plt"),
+    consult("test/Handlers/utils.plt"),
     consult("test/Handlers/roomsHandler.plt"),
     run_tests.
 
